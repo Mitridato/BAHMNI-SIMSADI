4658c9fbd061ceed4c6a77486e38e01b8dbf77ff
Nicolas Ricardo Muñoz Munizaga <163864879+Mitridato@users.noreply.github.com>
2024-07-19 03:15:41 -0400

    Update sendOrdersLRBORRAR.js
    agregado 2do intento

--- sendOrdersLRBORRAR.js
+++ sendOrdersLRBORRAR.js
@@ -31,3 +31,39 @@ angular.module('bahmni.clinical')
             controller: controller
         };
     });
+
+
+ ---v2 
+
+angular.module('EnvioOrdenes')
+    .directive('Sendorders',['orderService','labOrders','Sendorders','pacsOrders', function (orderService, labOrders, Sendorders, pacsOrders) {
+        var controller = function ($scope, $rootScope, ngDialog, visitActionsService) {
+            
+
+            $scope.isDataPresent = function () {
+                if (($scope.pacsOrders && $scope.pacsOrders.length == 0) || ($scope.labOrderSections && $scope.labOrderSections.length == 0)) {
+                    return $scope.$emit("no-data-present-event") && false;
+                }
+                return true;
+            };
+
+            $scope.downloadOrder = function (visitStartDate, visitUuid) {
+                $rootScope.$broadcast("event:downloadOrderFromDashboard", visitStartDate, visitUuid);
+            };
+            $scope.isOtherActiveSection = function (dateString) {
+                return dateString === Bahmni.Clinical.Constants.labOrderSection || dateString === Bahmni.Clinical.Constants.radiologyOrderSection;
+            };
+            $scope.shareOrders = function (visitStartDate, visitUuid) {
+                $rootScope.$broadcast("event:shareOrdersViaEmail", visitStartDate, visitUuid);
+            };
+        };
+
+        return {
+            templateUrl: "customDisplayControl/views/Emailtemplate.html",
+            scope: {
+                OrderSections: "=",
+                params: "="
+            },
+            controller: controller
+        };
+    }]);
